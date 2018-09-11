03f707b280ce48638ac619318ba4781c31a75a1e
Phillip Hwang <thehunter9709@gmail.com>
2018-09-11 16:48:52 -0400

    Fixed bugs

--- BaelinScraper.py
+++ BaelinScraper.py
@@ -33,8 +33,10 @@ with open("Sec 10-k Filings.csv") as csv_file:
             url = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK="\
                 + str(cik) + "&type=10&dateb=" + f_fdate + "&owner=exclude&count=100"
             r = requests.get(url)
-            print(url)
+            #print(url)
             br = BeautifulSoup(r.content, "html.parser")
+
+            #Find the document links
             for tableFile2 in br.find_all("table", {"class": "tableFile2"}):
                 num = 0
                 for tr in tableFile2.find_all("tr"):
@@ -44,28 +46,27 @@ with open("Sec 10-k Filings.csv") as csv_file:
                             break
                     num +=1
                 break
+
+            #Assemble the new link for the scraper to go to
             new_link = "https://www.sec.gov" + new_link
-            print(new_link)
-            #new_link = "https://www.sec.gov/Archives/edgar/data/1616543/000155837016003307/0001558370-16-003307-index.htm"
+            #print(new_link)
             r2 = requests.get(new_link)
+
+            #Go to the new link
             br2 = BeautifulSoup(r2.content, "html.parser")
 
+            #Find the first table
             for tableFile in br2.find_all("table", {"class": "tableFile"}):
                 num = 0
-                #print(tableFile)
+                new10k = ""
                 for tr in tableFile.find_all("tr"):
-                    #print(tr)
-                    #continue
-                    new10k = ""
+
                     td = tr.find_all("td")
-                    #print(td)
                     if(num == 1):
-                        #print(td)
-                        #print(td[1])
-                        if td[1].get_text() == "10-K" or td[3].get_text() == "10-Q":
+                        if "10-K" in td[1].get_text() or "10-Q" in td[3].get_text():
                             _10k = td[2].find("a").get("href")
                             if(".htm" in _10k):
-                                print("html file is: " + _10k)
+
                                 new10k = _10k
                                 new10k = "https://sec.gov" + new10k
                     num += 1
@@ -75,14 +76,16 @@ with open("Sec 10-k Filings.csv") as csv_file:
                         #print("txt file is: " + submission)
                     except:
                         continue
-                print("html file is: " + new10k)
+                #print("html file is: " + new10k)
+                #print("txt file is: " + submission)
                 print("txt file is: " + submission)
+                print("html file is: " + new10k)
                 with open("newfile.csv", "a") as write_file:
                     writer = csv.writer(write_file, delimiter=',')
                     writer.writerow(
                         [row[0], row[1], row[2], row[3], row[4], row[5], row[6], "https://sec.gov" + submission,
                          new10k])
-
+                break
 
 
 
